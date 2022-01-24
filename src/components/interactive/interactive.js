77c0ba80ca55a811db4e1d36e54a9e122f2037d7
Tommy Holt <tommy_holt@icloud.com>
2022-01-24 19:19:22 +0000

    Time up commit

--- src/components/interactive/interactive.js
+++ src/components/interactive/interactive.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "interactjs";
+import useSound from "use-sound";
+import correct from "../../sounds/correct.mp3"
 
 import css from "./interactive.module.css";
 import interact from "interactjs";
 
 export default function Interactive({ data }) {
   let position = { x: 0, y: 0 };
+
+  const [playOn] = useSound(correct, {volume: 0.25});
 
   interact(".text").draggable({
     listeners: {
@@ -21,9 +25,16 @@ export default function Interactive({ data }) {
     },
   });
 
+  interact("img").dropzone({
+    ondrop: function (e) {
+      console.log("Drop!")
+      playOn();
+    },
+  });
+
   return (
     <div className={css.container}>
-      <div className={css.image}>
+      <div className={css.image} class="image">
         <img
           className={css.image}
           src={data.length > 1 ? data[0].url : ""}
@@ -32,7 +43,7 @@ export default function Interactive({ data }) {
       <div class="text">
         <h1 className={css.text}>{data.length > 1 ? data[0].name : ""}</h1>
       </div>
-      <div className={css.image}>
+      <div className={css.image} class="image">
         <img
           className={css.image}
           src={data.length > 1 ? data[1].url : ""}
